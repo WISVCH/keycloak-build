106df42c48c7a681d9cd4df538c26b536abb0788
Robert van Dijk <robert@citroenvlinder17.nl>
2026-03-29 11:16:08 +0200

    Remove unused packages

--- src/main/java/ch/wisv/keycloak_custom_providers/claimmapper/models/api/Dienst2PeopleResponse.java
+++
@@ -1,9 +0,0 @@
-package ch.wisv.keycloak_custom_providers.claimmapper.models.api;
-
-import java.util.List;
-
-public class Dienst2PeopleResponse {
-
-    public int count;
-    public List<Person> results;
-}
